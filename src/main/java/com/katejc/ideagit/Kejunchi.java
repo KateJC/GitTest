261027647ac80075e090831f226a4eeddef6e315
kjc <18816794958@163.com>
2018-06-23 20:48:29 +0800

    Add:添加了Kejunchi类

---
+++ src/main/java/com/katejc/ideagit/Kejunchi.java
@@ -0,0 +1,10 @@
+package com.katejc.ideagit;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+
+public class Kejunchi {
+    public static void main(String[] args) {
+        System.out.println("hahahha ");
+    }
+}
+

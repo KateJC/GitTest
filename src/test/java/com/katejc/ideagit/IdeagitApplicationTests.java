ebc5a968141620786f928b91a1b43cc090bb82aa
kjc <18816794958@163.com>
2018-06-23 18:53:08 +0800

    Add:搭建项目 搭建了一个maven项目

---
+++ src/test/java/com/katejc/ideagit/IdeagitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.katejc.ideagit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdeagitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

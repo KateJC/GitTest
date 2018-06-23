1bf78401d54b14db45cc703007e40c3180739bbc
kjc <18816794958@163.com>
2018-06-23 20:59:35 +0800

    Add:增加了test分支，并增加testfenzhi类

--- src/main/java/com/katejc/ideagit/IdeagitApplication.java
+++ src/main/java/com/katejc/ideagit/IdeagitApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IdeagitApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(IdeagitApplication.class, args);
-	}
+    
+    public static void main(String[] args) {
+        SpringApplication.run(IdeagitApplication.class, args);
+    }
 }
